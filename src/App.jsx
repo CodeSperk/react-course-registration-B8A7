5af18f9de68e5b9c88df03c54afaf8d78360328d
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-03-14 20:38:29 +0600

    added functionality for insufficient credit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import Cart from './components/cart/Cart';
 import Courses from './components/courses/Courses';
 import Header from './components/header/Header';
 
+const totalCredit = 20;
+
 function App() {
   const [bookedCourses, setBookedCourses] = useState([]);
 
   const handleBookedCourses = (course) => {
+    // for existed course
     const isAlreadyExist = bookedCourses.find(bookedCourse=> bookedCourse.id === course.id);
 
     if(!isAlreadyExist){
       const newMarkedCourses = [...bookedCourses, course];
       setBookedCourses(newMarkedCourses);
+    }else {
+      return alert("Already Exist");
     }
+      // alert if total credit > 20
+      const credits = bookedCourses.reduce((p,c)=>p+c.credit,0);
+      if(credits + course.credit > totalCredit){
+        return alert("insufficient credits");
+      }
+  
   }
+
   return (
     <div className='max-w-[1440px] mx-auto px-4 md:px-8 lg:px-12 bg-slate-50 h-full pb-12'>
       <Header/>
